6bfe46a8848642395901930168f45a7f54156084
Álex Ávila <alexavila0128@gmail.com>
2025-10-04 16:20:53 +0200

    removing unused import

--- src/lib/event-modal.svelte
+++ src/lib/event-modal.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import type { CalendarEventExternal } from '@schedule-x/calendar';
 
 	let { calendarEvent }: { calendarEvent: CalendarEventExternal } = $props();
